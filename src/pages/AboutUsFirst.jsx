570e34304b029f07b9f4efc012111a9878c0140c
Marcelo Saldanha <marcelosfilho.fundador@yellowbagedu.com>
2026-05-13 21:41:35 -0300

    feat: finish layout of page AboutUs 01

--- src/pages/AboutUsFirst.jsx
+++ src/pages/AboutUsFirst.jsx
@@ -25,6 +25,22 @@ export function AboutUsFirst() {
                     ferramentas modernas para formar estudantes autônomos, 
                     curiosos e preparados para o futuro.
                 </p>
+                <p className={styles.contentTextDesktop}>
+                    A YellowBagEdu é uma startup paraense dedicada a transformar 
+                    a educação por meio da <span> Criatividade, Tecnologia e a habilidade do 
+                    Pensamento Computacional para transformar a educação.</span>  
+                    {" "}
+                    Nosso propósito é oferecer experiências 
+                    de aprendizagem acessíveis, divertidas e alinhadas à realidade 
+                    amazônica, aproximando crianças e jovens do mundo digital de um jeito 
+                    leve, prático e significativo.
+                    Acreditamos que o conhecimento precisa fazer sentido para quem 
+                    aprende. Por isso, desenvolvemos soluções que conectam 
+                    cultura local, desafios do cotidiano e ferramentas modernas de 
+                    ensino. Atuamos formando estudantes mais autônomos, curiosos e 
+                    preparados para um futuro onde a tecnologia é parte 
+                    essencial da vida.
+                </p>
             </div>
         </section>
     )
